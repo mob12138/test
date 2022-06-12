a557ebc8f0f95594002a9716d7276bb452acc4d8
mob12138 <ndyd12138@qq.com>
2022-06-12 19:06:27 +0800

    feat: 2

--- src/layout/headers/components/avatar.vue
+++ src/layout/headers/components/avatar.vue
@@ -17,7 +17,7 @@ import { useStore } from 'vuex'
 const store = useStore()
 
 const squareUrl = ref(
-  'https://img0.baidu.com/it/u=1056811702,4111096278&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'
+  'https://img2.baidu.com/it/u=4244269751,4000533845&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'
 )
 
 const logout = () => {
